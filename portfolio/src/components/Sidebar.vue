ca6f0c151cd2908e848fd9cd9ca4363a794c4bd4
Herbert Saladar <hrbrtsldr@gmail.com>
2021-07-13 21:34:29 +0800

    Add Assets and initial sidebar integration

--- portfolio/src/components/Sidebar.vue
+++ portfolio/src/components/Sidebar.vue
@@ -1,5 +1,18 @@
 <template>
-    <div class="decoration-clone bg-gradient-to-b from-yellow-400 to-red-500 text-transparent "> 
-        test
+    <div class=""> 
+        <img src="@/assets/images/avatar.png" alt="">
+        <h1> Herbert Saladar</h1>
+        <p> Developer</p>
+        <ul>
+            <li> About</li>
+            <li> Case studies </li>
+            <li>Skills</li>
+            <li>Services</li>
+            <li>Process</li>
+            <li>Reviews</li>
+            <li> Why Me</li>
+            <li> Contact</li>
+            <li> Download</li>
+        </ul>
     </div>
 </template>
--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="container mx-auto">
-    <Sidebar />
+    <Sidebar class="bg-blue-500 w-15"/>
+    <div class="section">
+
+    </div>
   </div>
 </template>
-
+styl
 <script>
 // @ is an alias to /src
 import Sidebar from '@/components/Sidebar.vue'
